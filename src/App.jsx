ec3ec22a68c8074d29975c83856007a47d0f3370
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-28 23:52:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MyButton from './component/MyButton'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello world</h1>
+     <MyButton/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/MyButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyButton() {
+  return (
+    <div>
+        <button>I'm Button</button>
+    </div>
+  )
+}
+
+export default MyButton
