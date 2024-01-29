9e865803ad8c9bb775df0a410c58d30c68c12658
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-29 11:43:57 +0700

    component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@
 import './App.css'
 import MyButton from './component/MyButton'
 
+function MyButton2() {
+  return (
+    <button>
+      I'm a real button 
+    </button>
+  );
+}
+
 function App() {
 
   return (
     <>
      <h1>Hello world</h1>
      <MyButton/>
+     <MyButton2/>
     </>
   )
 }
